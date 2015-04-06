71363ff15b3f2afbcfef3b367462d1da59872c06
wizzardo <wizzardobox@gmail.com>
2015-04-06 20:02:39 +0300

    add job class

---
+++ src/main/java/com/wizzardo/downloader/DownloadJob.java
@@ -0,0 +1,20 @@
+package com.wizzardo.downloader;
+
+import java.util.Map;
+
+/**
+ * Created by wizzardo on 04.04.15.
+ */
+public abstract class DownloadJob {
+
+    public String type;
+    public String name;
+    public Map params;
+    public int id;
+    public volatile DownloadStatus status = DownloadStatus.WAITING;
+
+
+    abstract void execute();
+
+    abstract Map<String, String> requiredParams();
+}
---
+++ src/main/java/com/wizzardo/downloader/DownloadStatus.java
@@ -0,0 +1,8 @@
+package com.wizzardo.downloader;
+
+/**
+ * Created by wizzardo on 04.04.15.
+ */
+public enum DownloadStatus {
+    DONE, IN_PROGRESS, FAILED, WAITING, CANCELLED
+}
