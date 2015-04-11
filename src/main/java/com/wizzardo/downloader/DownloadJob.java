5b8ec63a4431d080b2cc1367f34d6de5a30f812f
wizzardo <wizzardobox@gmail.com>
2015-04-11 13:16:42 +0300

    add Downloader

--- src/main/java/com/wizzardo/downloader/DownloadJob.java
+++ src/main/java/com/wizzardo/downloader/DownloadJob.java
@@ -1,7 +1,9 @@
 package com.wizzardo.downloader;
 
 import com.wizzardo.http.html.Tag;
+import com.wizzardo.tools.json.JsonObject;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,10 +20,17 @@ public abstract class DownloadJob {
     public int id;
     public volatile DownloadStatus status = DownloadStatus.WAITING;
 
+    private DownloadJobService downloadJobService;
+    private int progress;
+    private StringBuilder webLog = new StringBuilder();
 
     public abstract void execute();
 
     public abstract Map<String, String> requiredParams();
+
+    public void setDownloadJobService(DownloadJobService downloadJobService) {
+        this.downloadJobService = downloadJobService;
+    }
 
     public Tag renderInputFields() {
         Map<String, String> params = new HashMap<>();
@@ -35,5 +44,46 @@ public abstract class DownloadJob {
                             .add(span().clazz("key").text(it.getKey()))
                             .add(input().type("text").name(it.getKey()).placeholder(it.getValue().replace("\"", "")));
                 });
+    }
+
+    public void prepareWorkDir() {
+        deleteAllFiles();
+        getWorkDir().mkdirs();
+    }
+
+    public void deleteAllFiles() {
+        deleteAll(getWorkDir());
+    }
+
+    public File getWorkDir() {
+        return new File("/tmp/downloader/" + id);
+    }
+
+    private void deleteAll(File dir) {
+        if (dir.isDirectory())
+            for (File file : dir.listFiles()) {
+                if (file.isDirectory())
+                    deleteAll(file);
+                else
+                    file.delete();
+            }
+        dir.delete();
+    }
+
+    public boolean isCanceled() {
+        return status == DownloadStatus.CANCELLED;
+    }
+
+    public void log(Object ob) {
+        log(String.valueOf(ob));
+    }
+
+    public void log(String s) {
+        s = s.trim() + '\n';
+        webLog.append(s);
+    }
+
+    public void setStatus(DownloadStatus status) {
+        this.status = status;
     }
 }
---
+++ src/main/java/com/wizzardo/downloader/Downloader.java
@@ -0,0 +1,53 @@
+package com.wizzardo.downloader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Created by wizzardo on 11.04.15.
+ */
+public class Downloader extends Thread {
+    private BlockingQueue<DownloadJob> waiting = new LinkedBlockingQueue<>();
+    private DownloadJobService downloadJobService;
+
+    Downloader(DownloadJobService downloadJobService, BlockingQueue<DownloadJob> waiting) {
+        this.downloadJobService = downloadJobService;
+        this.waiting = waiting;
+        setDaemon(true);
+        setName("Downloader");
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                DownloadJob job = waiting.take();
+
+                job.setStatus(DownloadStatus.IN_PROGRESS);
+                try {
+                    job.prepareWorkDir();
+                    job.execute();
+                    if (!job.isCanceled())
+                        job.setStatus(DownloadStatus.DONE);
+                } catch (Exception e) {
+                    job.log(printStackTrace(e));
+                    job.setStatus(DownloadStatus.FAILED);
+                }
+                if (job.status == DownloadStatus.FAILED || job.status == DownloadStatus.CANCELLED)
+                    job.deleteAllFiles();
+            } catch (InterruptedException ignored) {
+            }
+        }
+    }
+
+    private String printStackTrace(Throwable t) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintWriter writer = new PrintWriter(out);
+        t.printStackTrace(writer);
+        writer.append('\n');
+        writer.close();
+        return out.toString();
+    }
+}
