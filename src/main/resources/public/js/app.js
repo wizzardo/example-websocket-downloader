74cbd26c232780871988c87792e12e3cfbaa234d
wizzardo <wizzardobox@gmail.com>
2015-07-05 19:54:28 +0300

    get rid of jquery

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,29 +1,44 @@
-
 var handlers = {}
 handlers.updateProgress = function (data) {
     if (data.progress < 100) {
-        $('#job_' + data.id + ' .progress .value').animate({width: data.progress + '%'}, 100);
+        lib.animate(lib('#job_' + data.id + ' .progress .value')[0], {width: data.progress + '%'}, 100);
     } else {
-        $('#job_' + data.id + ' .progress').hide();
-        $('#job_' + data.id + ' .download').show();
+        lib('#job_' + data.id + ' .progress')[0].style.display = 'none';
+        lib('#job_' + data.id + ' .download')[0].style.display = '';
     }
-}
+};
 
 handlers.updateLog = function (data) {
-    var textarea = $('#job_' + data.id + ' textarea')[0]
-    textarea.value = textarea.value + data.log
+    var textarea = lib('#job_' + data.id + ' textarea')[0];
+    textarea.value = textarea.value + data.log;
     scrollToBottom(textarea)
-}
+};
 
 handlers.updateStatus = function (data) {
-    $('#job_' + data.id).removeClass().addClass('job').addClass(data.cssClass);
-    $('#job_' + data.id + ' span.status').html(data.status);
+    var job = lib('#job_' + data.id)[0];
+    lib.removeClass(job);
+    lib.addClass(job, 'job');
+    lib.addClass(job, data.cssClass);
+    lib('#job_' + data.id + ' span.status')[0].innerHTML = data.status;
     if (data.status != 'IN_PROGRESS')
-        $('#job_' + data.id + ' span.cancel').hide();
-}
+        lib('#job_' + data.id + ' span.cancel')[0].style.display = 'none';
+};
 
 function toggleLog(id) {
-    $('#job_' + id + ' textarea').slideToggle(200)
+    var el = lib('#job_' + id + ' textarea')[0];
+    if (el.style.display == 'none') {
+        var to = window.getComputedStyle(el).height;
+        el.style.height = '0px';
+        el.style.display = '';
+        lib.animate(el, {height: to}, 200);
+    } else {
+        var from = el.style.height;
+        lib.animate(el, {height: '0px'}, 200, function () {
+            el.style.display = 'none';
+            el.style.height = from;
+        });
+    }
+    //$('#job_' + id + ' textarea').slideToggle(200);
     scrollToBottom(('#job_' + id + ' textarea')[0])
 }
 
@@ -32,7 +47,7 @@ function scrollToBottom(textarea) {
 }
 
 // Write your code in the same way as for native WebSocket:
-var ws = new WebSocket("ws://"+location.hostname+":"+location.port+"/ws");
+var ws = new WebSocket("ws://" + location.hostname + ":" + location.port + "/ws");
 ws.onopen = function () {
     console.log("open");
 //            ws.send("Hello");  // Sends a message.
@@ -40,7 +55,7 @@ ws.onopen = function () {
 ws.onmessage = function (e) {
     // Receives a message.
     console.log(e.data);
-    var data = JSON.parse(e.data)
+    var data = JSON.parse(e.data);
 
     handlers[data.command](data)
 };
@@ -51,4 +66,3 @@ ws.onclose = function () {
 function cancel(id) {
     ws.send('{"command":"cancel", "id":' + id + '}');
 }
-
